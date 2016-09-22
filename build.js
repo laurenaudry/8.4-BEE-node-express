b1e5288a1b12e72339c08eae389481707025d3a8
Lauren Johnson <laurenaudry1995@gmail.com>
2016-09-22 14:13:56 -0500

    npm installed

--- build.js
+++ build.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
-import Nav from './lib/Nav'
-import Home from './lib/Home'
-import NewArticle from './lib/NewArticle'
-import Article from './lib/Article'
-import EditArticle from './lib/EditArticle'
+
 
 render((
   <Router history={ hashHistory }>
-    <Route path="/nav" component={ Nav }>
-      <Route path="/" component={ Home }/>
-      <Route path="/new-article/:userID" component={ NewArticle }/>
-      <Route path="/edit-article/:articleID" component={ EditArticle } />
-      <Route path="/article/:articleID" component={ Article }/>
-    </Route>
+    <Route path="/" component={ Home }/>
   </Router>
 ), document.getElementById('app'))
