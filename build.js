33d82b3042aa04643dd777d46847a878c846ac51
Lauren Johnson <laurenaudry1995@gmail.com>
2016-09-22 15:06:13 -0500

    updated routes

--- build.js
+++ build.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
-
+import Main from './lib/Main'
+import Albums from './lib/Albums'
 
 render((
   <Router history={ hashHistory }>
-    <Route path="/" component={ Home }/>
+    <Route path="/" component={ Main }/>
+    <Route path="/Albums/:title" component={ Albums }/>
   </Router>
 ), document.getElementById('app'))
