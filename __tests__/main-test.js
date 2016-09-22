008ba2308236f6869fe58ad49a2f66683cb45b8f
Lauren Johnson <laurenaudry1995@gmail.com>
2016-09-22 15:57:48 -0500

    changed test

--- __tests__/main-test.js
+++ __tests__/main-test.js
@@ -1,11 +1,11 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import TestUtils from 'react-addons-test-utils'
-// import Main from '../lib/Main'
-//
-//
-// describe("Home", () => {
-//   it("should be a falsy test", () => {
-//     expect(true).toBe(true)
-//   })
-// })
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TestUtils from 'react-addons-test-utils'
+import Main from '../lib/Main'
+
+
+describe("Main", () => {
+  it("should be a falsy test", () => {
+    expect(true).toBe(true)
+  })
+})
