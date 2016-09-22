5abfa83d97424b1b515f7490444edcd2b41c8663
Lauren Johnson <laurenaudry1995@gmail.com>
2016-09-22 15:52:08 -0500

    added test folder

---
+++ __tests__/main-test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TestUtils from 'react-addons-test-utils'
+import Main from '../lib/Main'
+
+
+describe("Home", () => {
+  it("should be a falsy test", () => {
+    expect(true).toBe(true)
+  })
+})
