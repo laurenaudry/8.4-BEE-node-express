d6e41676a531f9d9fd3c4ed7719c342da7308f1f
Lauren Johnson <laurenaudry1995@gmail.com>
2016-09-23 14:10:09 -0500

    removed automock in individual test

--- __tests__/home-test.js
+++ __tests__/home-test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import TestUtils from 'react-addons-test-utils'
 import Home from '../lib/Home'
-jest.mock('../lib/Albums')
+
 
 describe("Home", () => {
   it("should pass finally", () => {
